65ea83213dc03a4c4a3b765d425d3d9a90849b92
NeuralSurvivor <arvindji0201@gmail.com>
2024-09-10 14:08:00 +0530

    add map and utils

---
+++ map.py
@@ -0,0 +1,96 @@
+campus_map = {
+    "path": {
+        "cse_dept": ["Zone_B", "cse_dept"],
+        "bb_dept": ["Zone_B", "bb_dept"],
+        "admin": ["Zone_A", "admin"],
+        "library": ["Zone_A", "Library"],
+        "ph_dept": ["Zone_C", "ph_dept"],
+        "mt_dept": ["Zone_C", "mt_dept"],
+    },
+    "dist": {
+        "cse_dept": 0,
+        "bb_dept": 0,
+        "admin": 0,
+        "library": 0,
+        "ph_dept": 0,
+        "mt_dept": 0,
+    },
+}
+
+cse_map = {
+    "path": {
+        "head": ["2nd_floor", "head_office"],
+        "bi": ["1st_fllor", "bi_office"],
+        "office": ["2nd_floor", "office"],
+    },
+    "dist":{
+        "head":0,
+        "bi":0,
+        "office":0,
+    }
+}
+
+bb_map = {
+    "path": {
+        "head": ["2nd_floor", "head_office"],
+        "bi": ["1st_fllor", "bi_office"],
+        "office": ["2nd_floor", "office"],
+    },
+    "dist":{
+        "head":0,
+        "bi":0,
+        "office":0,
+    }
+}
+
+ph_map = {
+    "path": {
+        "head": ["2nd_floor", "head_office"],
+        "bi": ["1st_fllor", "bi_office"],
+        "office": ["2nd_floor", "office"],
+    },
+    "dist":{
+        "head":0,
+        "bi":0,
+        "office":0,
+    }
+}
+
+mt_map = {
+    "path": {
+        "head": ["2nd_floor", "head_office"],
+        "bi": ["1st_fllor", "bi_office"],
+        "office": ["2nd_floor", "office"],
+    },
+    "dist":{
+        "head":0,
+        "bi":0,
+        "office":0,
+    }
+}
+
+admin_map = {
+    "path": {
+        "management": ["2nd_floor", "management_office"],
+        "bi": ["1st_fllor", "bi_office"],
+        "administration": ["2nd_floor", "admin_office"],
+    },
+    "dist":{
+        "management":0,
+        "bi":0,
+        "administration":0,
+    }
+}
+
+library_map = {
+    "path": {
+        "main": ["1st_floor", "main_library"],
+        "research": ["2nd_floor", "research_section"],
+        "archives": ["basement", "archives_room"],
+    },
+    "dist": {
+        "main": 0,
+        "research": 0,
+        "archives": 0,
+    }
+}
---
+++ utils.py
@@ -0,0 +1,7 @@
+speed_mode = {
+    'walk':1,
+    "cycle":2,
+    "car":3,
+}
+num_ci = 5
+prob_visitor = 0.2
